75cb9fd50dbcf41b0abed8b15712ba87d2bd63a2
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2025-10-28 13:42:14 -0400

    Added input for size of matrix

--- matrix/RunMatrix.java
+++ matrix/RunMatrix.java
@@ -2,5 +2,11 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+    int size;
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Enter a the size of your matrix");
+    size = input.nextInt();
+    input.close();
   }
 }
