12a588a52df378f9d6e1ea407058a0fba9898e9e
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2025-10-29 09:25:25 -0400

    Added constructor

--- matrix/Matrix.java
+++ matrix/Matrix.java
@@ -1,2 +1,15 @@
 public class Matrix {
+  private int[][] matrix;
+
+  public Matrix(int size){
+    matrix = new int[size][size];
+
+    System.out.println("You have made a matrix of " + size + " X " + size);
+
+    for(int y = 0; y < size; ++y){
+      for(int x = 0; x < size; ++x){
+        matrix[x][y] = 0;
+      }
+    }
+  }
 }
