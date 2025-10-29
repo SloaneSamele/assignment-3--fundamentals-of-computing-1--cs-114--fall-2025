e6daf0b72f693c38ba462e8aea7fef9670c333be
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2025-10-29 11:03:45 -0400

    Added matrix of user inputted size

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -1,4 +1,16 @@
+//import java.util.Scanner;
+
 public class Diamond {
   public static void main(String[] args) {
+    int size = 3, count = 1;
+    int [][] diamond;
+    // Scanner input = new Scanner(System.in);
+
+    // System.out.println("Enter a number: ");
+    // size = input.nextInt();
+    // input.close();
+
+    diamond = new int[size][size];
+
   }
 }
