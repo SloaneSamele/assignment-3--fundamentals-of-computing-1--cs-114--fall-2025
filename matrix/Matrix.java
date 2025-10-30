ead57028824869e53c73a5c5189131faa9281047
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2025-10-30 12:00:45 -0400

    Fixed output statement

--- matrix/Matrix.java
+++ matrix/Matrix.java
@@ -1,5 +1,7 @@
 public class Matrix {
   private int[][] matrix;
+  public static final String YELLOW =        "\033[33m\033[1m";
+  public static final String NORMAL =        "\033[0m";
 
   public Matrix(int size){
     matrix = new int[size][size];
@@ -19,21 +21,27 @@ public class Matrix {
     matrix[x1][y1] = matrix[x2][y2];
     matrix[x2][y2] = tempHolder;
   }
+
   public void printMatrix(){
     for(int y = 0; y < matrix.length; ++y) {
       for(int x = 0; x < matrix[y].length; ++x) {
-        System.out.print(matrix[x][y] + " ");
+        if((x + y) == matrix.length - 1){
+          System.out.printf("%s%4d%s    ", YELLOW, matrix[x][y], NORMAL);
+        }
+        else{
+        System.out.printf("%4d    ", matrix[x][y]);
+        }
       }
       System.out.println();
     }
   }
   public void flipMatrix(){
     int lastValue = matrix.length - 1;
+
     for(int y = 0; y < matrix.length / 2; ++y){
-      for(int x = 0; x < matrix.length; ++x){
       for(int x = 0; x < matrix[y].length; ++x){
         if((x + y) != lastValue){
-          swap(x, y, (lastValue - x), (lastValue -  y));
+          swap(x, y, (lastValue - x), (lastValue - y));
         }
       }
     }
