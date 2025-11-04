f36294224b4fc23b6635de5d731dbbbd7503218f
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2025-11-04 12:58:58 -0500

    Added diamond for odd

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -1,8 +1,12 @@
 //import java.util.Scanner;
+// import java.util.Scanner;
 
 public class Diamond {
   public static void main(String[] args) {
     int size = 3, count = 1;
+    int size = 4,
+        upperLimit,
+        lowerLimit;
     int [][] diamond;
     // Scanner input = new Scanner(System.in);
 
@@ -10,11 +14,22 @@ public class Diamond {
     // size = input.nextInt();
     // input.close();
 
-    diamond = new int[size][size];
-
-    for(int r = 0; r < diamond.length; ++r) {
-      for(int c = 0; c < diamond[r].length; ++c) {
-        System.out.print("*");
+    for(int row = 0; row < diamond.length; ++row) {
+      for(int col = 0; col < diamond[row].length; ++col) {
+        if(col <= upperLimit && col >= lowerLimit){
+          System.out.print("*");
+        }
+        else{
+          System.out.print(" ");
+        }
+      }
+      if(row < diamond.length / 2){
+        ++upperLimit;
+        --lowerLimit;
+      }
+      else{
+        --upperLimit;
+        ++lowerLimit;
       }
       System.out.println();
     }
