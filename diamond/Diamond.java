f15e41b4ad13a4d4db623bcfdfed4d9944d2cc68
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2025-11-13 14:15:25 -0500

    Condensed for loops

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 
 public class Diamond {
   public static void main(String[] args) {
-    int size = 8,
+    int size = 0,
         upperAstrixLimit,
         lowerAstrixLimit;
-    int [][] diamond;
+    boolean isEven = false;
 
     Scanner input = new Scanner(System.in);
 
@@ -20,88 +20,61 @@ public class Diamond {
     input.close();
 
     if(size % 2 == 0) {
-      diamond = new int[size + 1][size + 1];
-    }
-    else {
-      diamond = new int[size][size];
+      ++size;
+      isEven = true;
     }
 
     lowerAstrixLimit = size / 2;
     upperAstrixLimit = size / 2;
 
-    if(size % 2 != 0) {
-      for(int row = 0; row < diamond.length; ++row) {
-        for(int col = 0; col < diamond[row].length; ++col) {
-          if(col <= upperAstrixLimit && col >= lowerAstrixLimit) {
-            System.out.print("*");
-
-            if(col == upperAstrixLimit) {
+    for(int row = 0; row < size; ++row){
+      for(int col = 0; row < size; ++col){
+        if(isEven){
+          if(row == 0 || row == size - 1){
+            if(col >= lowerAstrixLimit && col <= upperAstrixLimit){
+              System.out.print("*");
 
               break;
-
+            }
+            else{
+              System.out.print("");
             }
           }
-          else {
+
+          if(col >= lowerAstrixLimit && col <= upperAstrixLimit){
+            if(col == size / 2){
+              continue;
+            }
+            System.out.print(" *");
+            if(col == upperAstrixLimit){
+              break;
+            }
+          }
+          else{
+            System.out.print("  ");
+          }
+        }
+        else{
+          if(col >= lowerAstrixLimit && col <= upperAstrixLimit){
+            System.out.print("*");
+            if(col == upperAstrixLimit){
+              break;
+            }
+          }
+          else{
             System.out.print(" ");
           }
         }
-
-        if(row < diamond.length / 2) {
-          ++upperAstrixLimit;
-          --lowerAstrixLimit;
-        }
-        else {
-          --upperAstrixLimit;
-          ++lowerAstrixLimit;
-        }
-
-        System.out.println();
       }
-  }
-  else {
-    for(int row = 0; row < diamond.length; ++row) {
-      for(int col = 0; col < diamond[row].length; ++col) {
-        if(row == 0 || row == diamond.length - 1) {
-          if(col >= lowerAstrixLimit && col <= upperAstrixLimit) {
-            System.out.print("*");
-
-            break;
-
-          }
-          else {
-            System.out.print("  ");
-          }
-        }
-        else {
-          if(col == diamond.length / 2){
-
-            continue;
-          }
-          if(col >= lowerAstrixLimit && col <= upperAstrixLimit) {
-            System.out.print(" *");
-            if(col == upperAstrixLimit) {
-
-              break;
-
-            }
-          }
-          else {
-            System.out.print("  ");
-          }
-        }
-      }
-
-      if(row < diamond.length / 2) {
+      System.out.println();
+      if(row < size / 2){
         ++upperAstrixLimit;
         --lowerAstrixLimit;
       }
-      else {
+      else{
         --upperAstrixLimit;
         ++lowerAstrixLimit;
       }
-
-      System.out.println();
     }
   }
- }
 }
